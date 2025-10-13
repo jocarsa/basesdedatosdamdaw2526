0cf85c364bdfb18fd2bb48b210d7d1aa92695506
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-10-13 11:00:55 +0200

    1

--- 002-Bases de datos relacionales/003-Tipos de datos/101-Ejercicios/002-Crear una tabla en la base de datos.sql
+++ 002-Bases de datos relacionales/003-Tipos de datos/101-Ejercicios/002-Crear una tabla en la base de datos.sql
@@ -7,3 +7,17 @@ CREATE TABLE clientes (
   apellidos VARCHAR(255),
   email VARCHAR(100)
 );
+
+Query OK, 0 rows affected (0,02 sec)
+
+
+mysql> SHOW TABLES;
++----------------------+
+| Tables_in_empresadam |
++----------------------+
+| clientes             |
++----------------------+
+1 row in set (0,00 sec)
+
+mysql> 
+
