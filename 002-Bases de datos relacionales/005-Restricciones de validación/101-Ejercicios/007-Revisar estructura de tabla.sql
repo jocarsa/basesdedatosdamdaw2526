147c76c9cef2215cc6cc6e24cab82af11666d061
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-10-15 14:10:50 +0200

    1

--- 002-Bases de datos relacionales/005-Restricciones de validación/101-Ejercicios/007-Revisar estructura de tabla.sql
+++ 002-Bases de datos relacionales/005-Restricciones de validación/101-Ejercicios/007-Revisar estructura de tabla.sql
@@ -15,4 +15,5 @@ RENAME COLUMN dni TO dninie;
 
 ERROR 3959 (HY000): Check constraint 'comprobar_dni_nie_letra' uses column 'dni', hence column cannot be dropped or renamed.
 
+ALTER TABLE clientes
 DROP CONSTRAINT comprobar_dni_nie_letra;
