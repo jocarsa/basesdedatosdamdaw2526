0b8cce6c9568c015fd0fc756f1020da9c0988ec5
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-10-15 13:48:54 +0200

    1

--- 002-Bases de datos relacionales/005-Restricciones de validación/101-Ejercicios/002-intento sanear parte 1.sql
+++ 002-Bases de datos relacionales/005-Restricciones de validación/101-Ejercicios/002-intento sanear parte 1.sql
@@ -13,3 +13,13 @@ SELECT * FROM clientes;
 ALTER TABLE clientes
   ADD CONSTRAINT comprobar_email
   CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$');
+  
+INSERT INTO clientes VALUES(
+  NULL,
+  '12345678A',
+  'nombre del cliente',
+  'apellidos del cliente',
+  'algo'
+);
+
+ERROR 3819 (HY000): Check constraint 'comprobar_email' is violated.
